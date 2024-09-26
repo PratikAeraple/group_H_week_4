24a548fb27647ca4ab415b2844c549ecd5343b4a
pratik aeraple <pratikaeraple@pratiks-MacBook-Air.local>
2024-09-26 14:45:34 -0400

    Adding isEven Code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module WINP2000/github.com/group_H_week_4
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Is 10 even?", IsEven(10))
+    fmt.Println("Is 7 even?", IsEven(7))
+}
